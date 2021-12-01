508902123ce5c0d20f811149f5170218d99c2d2e
niuez <niuez.graph@gmail.com>
2021-12-01 11:35:44 +0900

    [add] identifier parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate nom;
+extern crate log;
+
+pub mod parser;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/parser/identifier.rs
@@ -0,0 +1,28 @@
+use nom::IResult;
+use nom::branch::*;
+use nom::bytes::complete::*;
+use nom::character::complete::*;
+use nom::multi::*;
+use nom::sequence::*;
+
+#[derive(Debug)]
+pub struct Identifier {
+    name: String,
+}
+
+impl Identifier {
+    pub fn parse(s: &str) -> IResult<&str, Self> {
+        let (s, (head, tails)) = tuple((
+                alt((alpha1, tag("_"))),
+                many0(alt((alphanumeric1, tag("_"))))
+            ))(s)?;
+        let name = std::iter::once(head).chain(tails.into_iter()).collect::<Vec<_>>().join("");
+        Ok((s, Identifier { name }))
+    }
+}
+
+#[test]
+fn parse_identifier_test() {
+    println!("{:?}", Identifier::parse("a_12").unwrap());
+    println!("{:?}", Identifier::parse("abc_____").unwrap());
+}
